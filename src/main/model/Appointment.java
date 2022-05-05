50ca74e0c17a2e87e2bb460bdb6f5a667d8f686b
johnvanek <jvanimal2@gmail.com>
2022-05-05 15:49:06 -0400

    Add prototype models

---
+++ src/main/model/Appointment.java
@@ -0,0 +1,17 @@
+package main.model;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Appointment {
+    private int appointmentId;
+    private String title;
+    private String description;
+    private String location;
+    private String type;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+    private int customerId;
+    private int userId;
+    private int contactId;
+}
---
+++ src/main/model/FirstLevelDivision.java
@@ -0,0 +1,27 @@
+package main.model;
+
+public class FirstLevelDivision {
+    private int divisionId;
+    private String divisionName;
+    private int countryId;
+
+    public FirstLevelDivision(int divisionId, String divisionName, int countryId) {
+        this.divisionId = divisionId;
+        this.divisionName = divisionName;
+        this.countryId = countryId;
+    }
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+
+    public String getDivisionName() {
+        return divisionName;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+}
+
+
