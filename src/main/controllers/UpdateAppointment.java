0cb1d3ef11294a0a872e63e5741db519641809c9
John Vanek <62773153+JohnVanek@users.noreply.github.com>
2022-07-14 21:58:14 -0400

    Add UpdateAppointment.java

---
+++ src/main/controllers/UpdateAppointment.java
@@ -0,0 +1,71 @@
+package main.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class UpdateAppointment {
+
+    @FXML
+    private TextField TitleTextField;
+
+    @FXML
+    private TextField DescTextField;
+
+    @FXML
+    private TextField LocTextField;
+
+    @FXML
+    private TextField ConTextField;
+
+    @FXML
+    private Button SubmitNewButton;
+
+    @FXML
+    private TextField TypeTextField;
+
+    @FXML
+    private DatePicker StartDatePicker;
+
+    @FXML
+    private ComboBox<?> StartTimeComboBox;
+
+    @FXML
+    private ComboBox<?> EndTimeComboBox;
+
+    @FXML
+    private TextField CustIDTextField;
+
+    @FXML
+    private TextField UserIDTextField;
+
+    @FXML
+    void DisplayAppointments(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DisplayCustomers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DisplayRecords(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SubmitUpdate(MouseEvent event) {
+
+    }
+
+    @FXML
+    void TerminateSession(ActionEvent event) {
+
+    }
+
+}
