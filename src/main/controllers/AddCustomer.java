04c11160b7a98db0714be9e7c366b7b465da64b8
John Vanek <62773153+JohnVanek@users.noreply.github.com>
2022-07-16 09:53:02 -0400

    Add prefill date UpdateAppointment.java new todos AddCustomer.java

--- src/main/controllers/AddCustomer.java
+++ src/main/controllers/AddCustomer.java
@@ -11,7 +11,13 @@ import java.util.ResourceBundle;
 public class AddCustomer implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+        // TODO The combo box for countries in add needs to be backed in an observable List
+        //  Then it needs to be set here in initialized
+        //  The more difficult task is populating the provinces and states comboBox
+        //  the second combo box has to be disabled and only enabled after a country is picked in which case it then
+        //  Shows the correct observable list.
+        //  Probably going to need an observable list for each country, or some way to filter once list of counties
+        //  And return a new list
     }
 
     @FXML
--- src/main/controllers/UpdateAppointment.java
+++ src/main/controllers/UpdateAppointment.java
@@ -8,18 +8,23 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
 import main.DAO.models.Appointment;
 import main.utils.StageManager;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ResourceBundle;
 
 public class UpdateAppointment implements Initializable {
 
+
     //local class data
     public static Appointment appSelected;
     //FXML-ID'S*****************************************
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
     @FXML
     private TextField TitleTextField;
 
@@ -42,10 +47,10 @@ public class UpdateAppointment implements Initializable {
     private DatePicker StartDatePicker;
 
     @FXML
-    private ComboBox<?> StartTimeComboBox;
+    private ComboBox<LocalTime> StartTimeComboBox;
 
     @FXML
-    private ComboBox<?> EndTimeComboBox;
+    private ComboBox<LocalTime> EndTimeComboBox;
 
     @FXML
     private TextField CustIDTextField;
@@ -82,13 +87,37 @@ public class UpdateAppointment implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("The value of passing data is " + appSelected.getTitle());
-        populateData();
+        prefillData();
         // TODO when initialized pre-fill out the fields with the data that has been passed from appointments.java
-    }
-
-    private void populateData() {
-        //Get a reference to all the local fields
-        // Assign them the values from the appSelected
+        //  Copy over as much code as possible from Appointments add or move some of it into a separate class.
 
     }
+
+    private void prefillData() {
+        //Get that is stored.
+        Appointment data = appSelected;
+        //Get a reference to all the fields
+        // And assign the values
+        TitleTextField.setText(appSelected.getTitle());
+        DescTextField.setText(appSelected.getDescription());
+        LocTextField.setText(appSelected.getLocation());
+        TypeTextField.setText(appSelected.getType());
+        //This might need to be fixed uncertain how it converts time-wise
+        StartDatePicker.setValue(LocalDate.from(appSelected.getStartDateTime()));
+        StartTimeComboBox.setValue(LocalTime.from(appSelected.getStartDateTime()));
+        EndTimeComboBox.setValue(LocalTime.from(appSelected.getEndDateTime()));
+
+        CustIDTextField.setText(String.valueOf(appSelected.getCustomerId()));
+        UserIDTextField.setText(String.valueOf(appSelected.getUserId()));
+        ConTextField.setText(String.valueOf(appSelected.getContactId()));
+
+        //And assign these w/e there is a change from the fields in case they immediately resubmit
+    }
+
+    private void submitUpdate() {
+        //perform update sql operation
+        //only perform the update is the customer is available ignoring the current appointment
+    }
+
+
 }
