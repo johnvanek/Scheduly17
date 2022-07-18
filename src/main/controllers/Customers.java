20d567786de76f875ed82eda64f05f813914b3b8
John Vanek <62773153+JohnVanek@users.noreply.github.com>
2022-07-17 22:31:25 -0400

    Add delete functionality for Customers.java

--- src/main/controllers/Customers.java
+++ src/main/controllers/Customers.java
@@ -3,17 +3,26 @@ package main.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
 import main.DAO.models.Appointment;
 import main.DAO.models.Customer;
+import main.database.Connection;
 import main.utils.ObservableManager;
 import main.utils.StageManager;
 
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import static main.utils.ObservableManager.populateDataAppointmentLists;
+import static main.utils.ObservableManager.populateDataCustomerList;
 
 public class Customers implements Initializable {
     //FXML ID's
@@ -76,7 +85,53 @@ public class Customers implements Initializable {
 
     @FXML
     void DeleteCustomer(MouseEvent event) {
+        //The Alert Code
+        Alert confirmDelete = new Alert(Alert.AlertType.WARNING);
+        confirmDelete.setHeaderText("[WARNING] : [DELETION]");
+        confirmDelete.getButtonTypes().add(ButtonType.CANCEL);
+        confirmDelete.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
 
+        Customer selectedCustomer = null;
+        //If there is a current selection in the all view
+        if (CustomersTableView.getSelectionModel().getSelectedItem() != null) {
+            selectedCustomer = CustomersTableView.getSelectionModel().getSelectedItem();
+            confirmDelete.setContentText("[Warning!] - You are about to delete Customer[ID]: " +
+                    selectedCustomer.getCustomerId() + " . " +
+                    "Named " + selectedCustomer.getCustomerName() + " " +
+                    "[WARNING] - This will also delete all of the customers scheduled " +
+                    "appointments are you sure you wish to continue? " +
+                    "This action cannot be reversed!");
+            //TODO document this lambda
+            Customer effectivelyFinalSelectedCustomer = selectedCustomer;
+            confirmDelete.showAndWait().ifPresent(response -> {
+                if (response == ButtonType.OK) {
+                    //TODO first have to delete all the appointments from the Customer
+                    String query = "DELETE FROM appointments WHERE Customer_ID = " +
+                            effectivelyFinalSelectedCustomer.getCustomerId();
+
+                    String query2 = "DELETE FROM customers WHERE Customer_ID = " +
+                            effectivelyFinalSelectedCustomer.getCustomerId();
+
+                    try {
+                        //Try the first query
+                        PreparedStatement ps = Connection.getConnection().prepareStatement(query);
+                        System.out.println(ps.executeUpdate() + " database records have been deleted. From appointments.");
+                        //Attempt the second
+                        ps = Connection.getConnection().prepareStatement(query2);
+                        System.out.println(ps.executeUpdate() + " database records have been deleted. From customers.");
+                        //Cleanup
+                        ps.close();
+
+                        //When deletion does happen repopulate the Front end data.
+                        populateDataAppointmentLists();
+                        populateDataCustomerList();
+                    } catch (SQLException e) {
+                        System.out.println("There was a problem trying to perform a deletion Customers Page");
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        }
     }
 
     @Override
