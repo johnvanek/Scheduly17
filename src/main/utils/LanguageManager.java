3ee4e6b3837dfbec74e34f6c995fb04a83a47c06
John Vanek <62773153+JohnVanek@users.noreply.github.com>
2022-07-28 12:09:24 -0400

    add LanguageManager.java javadoc

--- src/main/utils/LanguageManager.java
+++ src/main/utils/LanguageManager.java
@@ -3,19 +3,45 @@ package main.utils;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+/**
+ * A utility class that serves to assist getting the resource bundle for a language resource
+ */
 public final class LanguageManager {
+
+    /**
+     * A string representation of the language package name
+     */
+    private static final String resourceLocation = "Lang";
+    /**
+     * A locale representing the default locale set on the user's computer
+     */
+    private static final Locale locale = Locale.getDefault();
+    /**
+     * A resource bundle that represents the package containing the available languages key value pairs
+     */
+    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(resourceLocation, locale);
+
+    /**
+     * Private constructor so that this object cannot be instantiated
+     */
     private LanguageManager() {
     }
 
-    private static final String resourceLocation = "Lang";
-    private static final Locale locale = Locale.getDefault();
-
-    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(resourceLocation, locale);
-    public static Locale getLocale(){
+    /**
+     * Returns the user's system set locale
+     *
+     * @return a locale representing the user's system set locale
+     */
+    public static Locale getLocale() {
         return locale;
     }
 
-    public static ResourceBundle getResourceBundle(){
+    /**
+     * Returns a resource bundle that represents the set of key value pairs for the available languages
+     *
+     * @return a resource bundle representing a language's key value pairs
+     */
+    public static ResourceBundle getResourceBundle() {
         return resourceBundle;
     }
 }
