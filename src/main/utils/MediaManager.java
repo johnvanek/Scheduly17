eaf1c9e0c06d97bab80ae4959f41e97c4d53ba7e
John Vanek <62773153+JohnVanek@users.noreply.github.com>
2022-07-28 12:16:43 -0400

    add MediaManager javadoc

--- src/main/utils/MediaManager.java
+++ src/main/utils/MediaManager.java
@@ -6,22 +6,49 @@ import main.controllers.Login;
 
 import java.util.Objects;
 
+/**
+ * A utility class that manages all the functionality for setting up a media view in javaFx
+ */
 public final class MediaManager {
+    /**
+     * The location of the resource to be shown represented as a string path
+     */
+    public static final String resource = Objects.requireNonNull(Login.class.getResource("/Images/notesWriting.mp4")).toString();
+    /**
+     * A mediaPlayer for playing media from the javaFx libary
+     */
+    private static MediaPlayer mediaPlayer;
+
+    /**
+     * Private constructor so that this object cannot be instantiated
+     */
     private MediaManager() {
 
     }
 
-    public static final String resource = Objects.requireNonNull(Login.class.getResource("/Images/notesWriting.mp4")).toString();
-    private static MediaPlayer mediaPlayer;
-
+    /**
+     * Returns a reference to the current mediaPlayer or a null value
+     *
+     * @return an active mediaPlayer or null
+     */
     public static MediaPlayer getMediaPlayer() {
         return mediaPlayer;
     }
 
+    /**
+     * Assigns an instantiated mediaPlayer to the local reference in MediaManager so that the methods within apply to the
+     * instantiated object.
+     *
+     * @param mediaPlayer A mediaPlayer for playing media from the javaFx libary
+     */
     static void setMediaPlayer(MediaPlayer mediaPlayer) {
         MediaManager.mediaPlayer = mediaPlayer;
     }
 
+    /**
+     * Initializes the mediaPlayer. If you here no volume this is not an error the volume is muted, autoplay is enabled
+     * and the video will loop indefinitely until the scene is transitioned off the active scene.
+     */
     public static void initMediaPlayer() {
         MediaPlayer mediaPlayer = new MediaPlayer(new Media(resource));
         mediaPlayer.isMute();
