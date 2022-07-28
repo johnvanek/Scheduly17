1a39a73d20d76243751e8107abf30ebf2f5b179f
John Vanek <62773153+JohnVanek@users.noreply.github.com>
2022-07-28 12:19:32 -0400

    add Misc javadoc

--- src/main/utils/Misc.java
+++ src/main/utils/Misc.java
@@ -1,10 +1,22 @@
 package main.utils;
 
+/**
+ * A utility class that serves to store miscellaneous useful functions.
+ */
 public final class Misc {
 
+    /**
+     * Private constructor so that this object cannot be instantiated
+     */
     private Misc() {
     }
 
+    /**
+     * Given a string this method will uppercase the first character and send it back to the caller.
+     *
+     * @param lowerCaseString A string that is presumed to be lowercase
+     * @return a string with the first char upper-cased
+     */
     public static String makeFirstLetterUppercase(String lowerCaseString) {
         return lowerCaseString.substring(0, 1).toUpperCase() + lowerCaseString.substring(1);
     }
