a50937635c4dd191a436ad30bec9c6e70b507efc
John Vanek <62773153+JohnVanek@users.noreply.github.com>
2022-07-28 09:31:45 -0400

    add javadoc for CustomerMonths

--- src/main/DAO/models/CustomerMonths.java
+++ src/main/DAO/models/CustomerMonths.java
@@ -2,12 +2,31 @@ package main.DAO.models;
 
 import java.time.Month;
 
+/**
+ * This class is a data model that represents the number of customer specific appointments that are in a month
+ */
 public class CustomerMonths {
 
+    /**
+     * unique int customer id represents a customer
+     */
     private final int customerID;
+    /**
+     * enum month representing one of the twelve months
+     */
     private final Month month;
-    private int totalAppointments;
+    /**
+     * total of all appointments in a month for a given customer id and a given month
+     */
+    private final int totalAppointments;
 
+    /**
+     * The constructor for the data model object customerMonths
+     *
+     * @param customerID        unique int customer id represents a customer
+     * @param month             enum month representing one of the twelve months
+     * @param totalAppointments total of all appointments in a month for a given customer id and a given month
+     */
     public CustomerMonths(int customerID, Month month, int totalAppointments) {
         this.customerID = customerID;
         this.month = month;
